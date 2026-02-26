e84c6d88f417d895fa927ef3637cf2544706c371
vLevel <captainmatteofsm@gmail.com>
2026-02-26 07:57:52 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCRrmVsqWUIY0jaCE0tAmPJVZxygKX1uz0",
+  authDomain: "vlevel-8f884.firebaseapp.com",
+  projectId: "vlevel-8f884",
+  storageBucket: "vlevel-8f884.firebasestorage.app",
+  messagingSenderId: "728207102095",
+  appId: "1:728207102095:web:9f7d1e3ff1614b7505e0fa",
+  measurementId: "G-NPJGG0TF4X"
+};
